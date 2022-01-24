bb04f04aba7077c9985db7419e1dc74ffe928759
Bob Bobov <bobyy@yandex.ru>
2022-01-24 23:13:44 +0200

    Add project files.

---
+++ test/HalfDivider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace test
+{
+    public class HalfDivider
+    {
+        private int[] array = { 1, 4, 7, 4 };
+        //private int[] array = { 2, 5, 7 ,14, 58};
+        public HalfDivider()
+        {
+            var result = CanBeDevidedByHalf(array);
+            Console.WriteLine($"The Collection can be devided by half: { result }");
+        }
+
+        private bool CanBeDevidedByHalf(int[] array)
+        {
+            return CanBeDevidedByHalfInner(array, new Stack<int>());
+        }
+        private bool CanBeDevidedByHalfInner(int[] array,Stack<int> print ,int sum = 0,int index = 0)
+        {
+            bool result = false;
+            if (sum > array.Sum() - sum || index == array.Length)
+            {
+                return false;
+            }
+            else if (sum == array.Sum() - sum)
+            {
+                PrintArray(print.ToList());
+                return true;
+            }
+            else
+            {
+                for (int i = index; i < array.Length; i++)
+                {
+                    print.Push(array[i]);
+                    result = result || CanBeDevidedByHalfInner(array, print, sum + array[i], index + 1);
+                    print.Pop();
+                }
+                return result;
+            }
+        }
+
+        private void PrintArray(List<int> list)
+        {
+            list.ForEach(x => { Console.Write(x + " "); });
+            Console.Write(" = ");
+            array.Where(x => !list.Contains(x)).ToList().ForEach(x => { Console.Write(x + " "); });
+            Console.WriteLine();
+        }
+
+    }
+}
+
---
+++ test/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int distance = 5;
+            //int[] validSteps = { 2, 3, 5 };
+            //int ways = DogToBone(distance, validSteps);
+            //Console.WriteLine($"number of ways: { ways}");
+            
+            HalfDivider halfDivider = new HalfDivider();
+            
+            Console.ReadLine();
+        }
+
+        static int DogToBone(int distance, int[] validSteps)
+        {
+            return Steps(distance, validSteps, new List<int>());
+        }
+
+        static int Steps(int distance, int[] validSteps, List<int> print)
+        {
+            if (distance == 0)
+            {
+                PrintColl(print);
+                return 1;
+            }
+            else if (distance < 0)
+            {
+                //print.RemoveAt(print.Count - 1);
+                return 0;
+            }
+            int count = 0;
+            for (int i = 0; i < validSteps.Length; i++)
+            {
+                int step = validSteps[i];
+                print.Add(step);
+                count += Steps(distance - step, validSteps, print);
+                print.RemoveAt(print.Count - 1);
+            }
+            return count;
+        }
+        private static void PrintColl(List<int> print)
+        {
+            print.ForEach(x => { Console.Write(x + " "); });
+            Console.WriteLine();
+        }
+
+    }
+    enum OperationEmum
+    {
+        APPEND = 1,
+        MOVE,
+        DELETE,
+        SELECT,
+        CUT,
+        PASTE,
+        UNDO,
+        REDO,
+        SWITCH
+    }
+}
+
