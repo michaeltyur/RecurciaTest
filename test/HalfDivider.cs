8a61bc692a8d6a5c90ace6db9fd79f1142faf45e
Bob Bobov <bobyy@yandex.ru>
2022-01-25 16:12:37 +0200

    version 1.1

--- test/HalfDivider.cs
+++ test/HalfDivider.cs
@@ -7,51 +7,64 @@ namespace test
 {
     public class HalfDivider
     {
-        //private int[] array = { 1, 4, 7, 10 };
-        //private int[] array = { 3,2,4,7,12};
-        private int[] array = { 3, 2, 4, 7 };
-        //private int[] array = { 2, 5, 7, 14, 18 };
-        //private int[] array = { 33, 11, 22 };
-        //private int[] array = { 56, 11, 22 };
         public HalfDivider()
         {
-            string result = CanBeDevidedIntoTwo(array, new Stack<int>()) ? "Yes" : "No";
+            //int[] array = { 1, 4, 7, 10 }; //yes
+            //int[] array = { 3, 2, 4, 7, 12 }; //yes
+            //int[] array = { 3, 2, 4, 7 };// L no
+            //int[] array = { 3, 2, 4, 7, 12 };// L yes
+            //int[] array = { 2, 5, 7, 14, 18 }; // yes
+            //int[] array = { 33, 11, 22 }; //yes
+            // int[] array = { 56, 11, 22 }; //no
+            int[] array = { 11, 11, 22 ,33}; //no
+            string result = CanBeDevidedIntoTwo(array, array, new Stack<int>()) ? "Yes" : "No";
 
             Console.WriteLine($"The Collection can be devided into two: { result }");
         }
 
-        private bool CanBeDevidedIntoTwo(int[] array, Stack<int> print, int sum = 0, int index = 0)
+        private bool CanBeDevidedIntoTwo(int[] array, int[] originArray, Stack<int> print, int sum = 0)
         {
             bool result = false;
-            if (sum > array.Sum() - sum)
+            if (sum > originArray.Sum() - sum)
             {
                 return false;
             }
-            else if (sum == array.Sum() - sum)
+            else if (sum == originArray.Sum() - sum)
             {
-                PrintArray(print.ToList());
+                PrintArray(print.ToList(), originArray);
                 return true;
             }
             else
             {
-                for (int i = index; i < array.Length; i++)
+                for (int i = 0; i < array.Length; i++)
                 {
                     print.Push(array[i]);
-                    result = result || CanBeDevidedIntoTwo(array, print, sum + array[i], index + 1) ;
+                    result = result || CanBeDevidedIntoTwo(array.Skip(i + 1).ToArray(), originArray, print, sum + array[i]);
                     print.Pop();
                 }
                 return result;
             }
         }
 
-        private void PrintArray(List<int> list)
+        private void PrintArray(List<int> list, int[] originArray)
         {
             list.ForEach(x => { Console.Write(x + " "); });
             Console.Write("= ");
-            array.Where(x => !list.Contains(x)).ToList().ForEach(x => { Console.Write(x + " "); });
+            originArray.Where(x => !list.Contains(x)).ToList().ForEach(x => { Console.Write(x + " "); });
             Console.WriteLine();
         }
 
+    }
+    public static class ExtensionMethods
+    {
+        public static bool IsHalf(this int sum, int[] array)
+        {
+            return sum == array.Sum() - sum;
+        }
+        public static bool Bigger(this int sum, int[] array)
+        {
+            return sum > array.Sum() - sum;
+        }
     }
 }
 
