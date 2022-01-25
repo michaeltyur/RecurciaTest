d05f509665096269e2cec555d62a0ef14bc67bbc
Bob Bobov <bobyy@yandex.ru>
2022-01-25 09:57:02 +0200

    fdf

--- test/HalfDivider.cs
+++ test/HalfDivider.cs
@@ -8,14 +8,15 @@ namespace test
     public class HalfDivider
     {
         //private int[] array = { 1, 4, 7, 10 };
-        private int[] array = { 2, 5, 7 ,14, 18};
+        private int[] array = { 2, 5, 7, 14, 18 };
         public HalfDivider()
         {
-            var result = CanBeDevidedIntoTwo(array,new Stack<int>());
+            string result = CanBeDevidedIntoTwo(array, new Stack<int>()) ? "Yes" : "No";
+
             Console.WriteLine($"The Collection can be devided into two: { result }");
         }
 
-        private bool CanBeDevidedIntoTwo(int[] array,Stack<int> print ,int sum = 0,int index = 0)
+        private bool CanBeDevidedIntoTwo(int[] array, Stack<int> print, int sum = 0, int index = 0)
         {
             bool result = false;
             if (sum > array.Sum() - sum)
@@ -32,7 +33,7 @@ namespace test
                 for (int i = index; i < array.Length; i++)
                 {
                     print.Push(array[i]);
-                    result = result || CanBeDevidedByHalfInner(array, print, sum + array[i], index + 1);
+                    result = result || CanBeDevidedIntoTwo(array, print, sum + array[i], index + 1);
                     print.Pop();
                 }
                 return result;
@@ -42,7 +43,7 @@ namespace test
         private void PrintArray(List<int> list)
         {
             list.ForEach(x => { Console.Write(x + " "); });
-            Console.Write(" = ");
+            Console.Write("= ");
             array.Where(x => !list.Contains(x)).ToList().ForEach(x => { Console.Write(x + " "); });
             Console.WriteLine();
         }
